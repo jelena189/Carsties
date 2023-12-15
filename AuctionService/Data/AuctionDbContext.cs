47669aee6dc6fc3849dbb41eabfb3e5b358a1006
jelena.cvetkovic <jelena.cvetkovic@primeholding.com>
2023-12-15 15:26:21 +0100

    End of section 2

---
+++ AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data
+{
+    public class AuctionDbContext : DbContext
+    {
+        public AuctionDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        // will create the table for both items and auction bc they are related, but it wont give items a pluralized name (that's why we need to specify it in the Item class in the [Table] decorator)
+        public DbSet<Auction> Auctions { get; set; }
+    }
+}
